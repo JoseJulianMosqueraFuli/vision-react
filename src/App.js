e1a7842c3156a99313eabce71b7113ef579ba623
Jose Mosquera <jojumofu@gmail.com>
2023-11-09 11:43:51 -0500

    chore: add initial changes for app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [url, setUrl] = useState('');
   const value = 'World';
-  return <div>Hello {value}</div>;
+
+  const handleInputChange = (event) => {
+    setUrl(event.target.value);
+  };
+
+  const handleAnalyzeClick = () => {
+    // Lógica para analizar la URL
+    console.log(`Analizando la URL: ${url}`);
+  };
+
+  const handleExecuteClick = () => {
+    // Lógica para ejecutar la acción con la URL
+    console.log(`Ejecutando acción con la URL: ${url}`);
+  };
+
+  return (
+    <div>
+      <h1>Computer Vision</h1>
+      <div>
+        <label><b>Insert URL or type prompt:</b></label>
+        <br />
+        <input type="text" value={url} onChange={handleInputChange} />
+        <p>{/* Comentario opcional */}</p>
+        <button onClick={handleAnalyzeClick}>Analyze</button>
+        &nbsp;
+        <button onClick={handleExecuteClick}>Execute</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
